500ee81a47a846ddfaefcd24281b5ed8492591f2
Adrianpede <pedemonteadrian@gmail.com>
2023-02-19 19:23:22 -0300

    jornal-app activar una nota

--- src/journal/components/SideBarItem.jsx
+++ src/journal/components/SideBarItem.jsx
@@ -1,9 +1,16 @@
 import { TurnedInNot } from '@mui/icons-material'
 import { Grid, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material'
 import { useMemo } from 'react';
+import { useDispatch } from 'react-redux';
+import { setActiveNote } from '../../store/journal/journalSlice';
 
 
-export const SideBarItem = ({title='', body, id}) => {
+export const SideBarItem = ({title='', body, id, date, imageUrls= []}) => {
+    const dispatch = useDispatch();
+
+    const onClickNote = ()=> {
+        dispatch(setActiveNote({title, body, id, date, imageUrls}))
+    }
 
     const newTitle = useMemo( ()=> {
         return title.length > 17
@@ -13,7 +20,7 @@ export const SideBarItem = ({title='', body, id}) => {
     }, [title])
   return (
     <ListItem  disablePadding>
-        <ListItemButton>
+        <ListItemButton onClick={onClickNote}>
         <TurnedInNot/>
         <ListItemIcon/>
 
